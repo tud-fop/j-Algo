19050dcea2ecb879631c2c7fa8b76e3aef31f155
styjdt <styjdt>
2005-08-30 09:57:56 +0000

    code formatting gui elements externalized to JAlgoGUIConnector

--- src/org/jalgo/main/util/Storage.java
+++ src/org/jalgo/main/util/Storage.java
@@ -1,23 +1,24 @@
-/* j-Algo - j-Algo is an algorithm visualization tool, especially useful for
- * students and lecturers of computer sience. It is written in Java and
- * platform independant. j-Algo is developed with the help of Dresden
- * University of Technology.
- *
+/*
+ * j-Algo - j-Algo is an algorithm visualization tool, especially useful for
+ * students and lecturers of computer sience. It is written in Java and platform
+ * independant. j-Algo is developed with the help of Dresden University of
+ * Technology.
+ * 
  * Copyright (C) 2004-2005 j-Algo-Team, j-algo-development@lists.sourceforge.net
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 /*
@@ -33,10 +34,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Shell;
 import org.jalgo.main.IModuleConnector;
 import org.jalgo.main.IModuleInfo;
+import org.jalgo.main.JAlgoGUIConnector;
 import org.jalgo.main.Jalgo;
 
 /**
@@ -48,16 +48,15 @@ public class Storage {
 		return load(filename, null);
 	}
 
-	public static boolean load(
-		String filename,
-		IModuleConnector currentInstance) {
+	public static boolean load(String filename, IModuleConnector currentInstance) {
 		byte[] buf = null;
 
 		// Create InputStream
 		FileInputStream in = null;
 		try {
 			in = new FileInputStream(filename);
-		} catch (FileNotFoundException e) {
+		}
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
@@ -66,10 +65,10 @@ public class Storage {
 			buf = new byte["jalgo".getBytes().length]; //$NON-NLS-1$
 			in.read(buf, 0, "jalgo".getBytes().length); //$NON-NLS-1$
 			if (!(new String(buf).equals("jalgo"))) { //$NON-NLS-1$
-				Shell shell = new Shell();
-				MessageDialog.openError(shell, Messages.getString("Storage.Could_not_open_file_4"), //$NON-NLS-1$
-				Messages.getString("Storage.File_not_valid_5")); //$NON-NLS-1$
-				shell.dispose();
+				JAlgoGUIConnector.getInstance().showErrorMessage(
+					Messages.getString("main", "Storage.Could_not_open_file_4") + //$NON-NLS-1$
+					System.getProperty("line.separator") +
+					Messages.getString("main", "Storage.File_not_valid_5")); //$NON-NLS-1$
 				return false;
 			}
 			buf = null;
@@ -78,7 +77,7 @@ public class Storage {
 			int nameLength = in.read();
 			int versionLength = in.read();
 
-			// Read Header	
+			// Read Header
 			buf = new byte[nameLength];
 			in.read(buf, 0, nameLength);
 			String name = new String(buf);
@@ -86,17 +85,17 @@ public class Storage {
 			in.read(buf, 0, versionLength);
 			buf = null;
 
-			//get modNumber of module corresponding to this file
+			// get modNumber of module corresponding to this file
 			int modNumber = -1;
 			LinkedList moduleInfos = Jalgo.getJalgoMain().getKnownModuleInfos();
 			for (int i = 0; i < moduleInfos.size(); i++) {
-				if (((IModuleInfo) moduleInfos.get(i)).getName().equals(name))
-					modNumber = i;
+				if (((IModuleInfo)moduleInfos.get(i)).getName().equals(name)) modNumber = i;
 			}
 			if (modNumber < 0) {
-				Shell shell = new Shell();
-				MessageDialog.openError(shell, "Cannot open file", "Could not find a J-Algo module for this file.");
-				shell.dispose();
+				JAlgoGUIConnector.getInstance().showErrorMessage(
+					Messages.getString("main", "Storage.Could_not_open_file_4") + //$NON-NLS-1$
+					System.getProperty("line.separator") +
+					Messages.getString("main", "Storage.Module_not_present")); //$NON-NLS-1$
 				return false;
 			}
 
@@ -109,14 +108,18 @@ public class Storage {
 
 			// give outStream to Module
 			ByteArrayInputStream outStream = new ByteArrayInputStream(buf);
-			if (currentInstance == null) throw new NullPointerException("currentInstance is null");
+			if (currentInstance == null)
+				throw new NullPointerException("currentInstance is null");
 			currentInstance.setDataFromFile(outStream);
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		}
+		finally {
 			try {
 				in.close();
-			} catch (IOException e) {
+			}
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
@@ -127,31 +130,21 @@ public class Storage {
 	public static boolean save(String filename) {
 		// Generate Headers
 		int nameLength =
-			Jalgo
-				.getCurrentModule()
-				.getModuleInfo()
-				.getName()
-				.getBytes()
-				.length;
+			Jalgo.getCurrentModule().getModuleInfo().getName().getBytes().length;
 		int versionLength =
-			Jalgo
-				.getCurrentModule()
-				.getModuleInfo()
-				.getVersion()
-				.getBytes()
-				.length;
+			Jalgo.getCurrentModule().getModuleInfo().getVersion().getBytes().length;
 
 		// Create OutputStream
 		FileOutputStream out = null;
 		try {
 			out = new FileOutputStream(filename);
-
-		} catch (FileNotFoundException e) {
-			Shell shell = new Shell();
-			MessageDialog.openError(shell, Messages.getString("Storage.Could_not_write_file_7"), //$NON-NLS-1$
-			Messages.getString("Storage.Could_not_write_the_file._n_8") + //$NON-NLS-1$
-			Messages.getString("Storage.Maybe_out_of_diskspace_9")); //$NON-NLS-1$
-
+		}
+		catch (FileNotFoundException e) {
+			JAlgoGUIConnector.getInstance().showErrorMessage(
+				Messages.getString("main", "Storage.Could_not_write_file_7") + //$NON-NLS-1$
+				System.getProperty("line.separator") +
+				Messages.getString("main", "Storage.Could_not_write_the_file._n_8") + //$NON-NLS-1$
+				Messages.getString("main", "Storage.Maybe_out_of_diskspace_9")); //$NON-NLS-1$
 			e.printStackTrace();
 		}
 
@@ -163,21 +156,20 @@ public class Storage {
 			out.write(
 				Jalgo.getCurrentModule().getModuleInfo().getName().getBytes());
 			out.write(
-				Jalgo
-					.getCurrentModule()
-					.getModuleInfo()
-					.getVersion()
-					.getBytes());
+				Jalgo.getCurrentModule().getModuleInfo().getVersion().getBytes());
 
 			// Write module data
 			out.write(Jalgo.getCurrentModule().getDataForFile().toByteArray());
 
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			e.printStackTrace();
-		} finally {
+		}
+		finally {
 			try {
 				out.close();
-			} catch (IOException e) {
+			}
+			catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
