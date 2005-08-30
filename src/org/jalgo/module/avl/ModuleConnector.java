b0dd6be07e05516414144a6089c977082908b786
styjdt <styjdt>
2005-08-30 10:07:40 +0000

    code formatting strings externalized some corrections to avoid warnings new methods implemented from IModuleConnector

--- src/org/jalgo/module/avl/ModuleConnector.java
+++ src/org/jalgo/module/avl/ModuleConnector.java
@@ -1,20 +1,24 @@
-/* j-Algo - j-Algo is an algorithm visualization tool, especially useful for students and lecturers of computer sience. It is written in Java and platform independant. j-Algo is developed with the help of Dresden University of Technology.
- *
+/*
+ * j-Algo - j-Algo is an algorithm visualization tool, especially useful for
+ * students and lecturers of computer sience. It is written in Java and platform
+ * independant. j-Algo is developed with the help of Dresden University of
+ * Technology.
+ * 
  * Copyright (C) 2004-2005 j-Algo-Team, j-algo-development@lists.sourceforge.net
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
 /* Created on 12.04.2005 */
@@ -35,45 +39,45 @@ import org.eclipse.jface.window.ApplicationWindow;
 import org.eclipse.swt.widgets.Composite;
 import org.jalgo.main.IModuleConnector;
 import org.jalgo.main.IModuleInfo;
+import org.jalgo.main.JAlgoGUIConnector;
+import org.jalgo.main.util.Messages;
 import org.jalgo.module.avl.datastructure.Node;
 import org.jalgo.module.avl.datastructure.SearchTree;
 import org.jalgo.module.avl.gui.GUIController;
 
 /**
- * This class provides the bridge between the main program and the current instance
- * of the AVL module. It handles the references of several objects and provides
- * some getters used in the main program.
+ * This class provides the bridge between the main program and the current
+ * instance of the AVL module. It handles the references of several objects and
+ * provides some getters used in the main program.
  * 
  * @author Alexander Claus
  */
 public class ModuleConnector
 implements IModuleConnector {
 
-	//gui components
+	// gui components
 	private SubMenuManager menuManager;
 	private SubToolBarManager toolBarManager;
 	private SubStatusLineManager statusLineManager;
 
-	//singleton references for each module opened
+	// singleton references for each module opened
 	private ModuleInfo moduleInfo;
 
 	private SearchTree tree;
 	private Controller controller;
 	private GUIController gui;
+	private int saveStatus;
 
-    /**
+	/**
 	 * Constructs a <code>ModuleConnector</code> object for the AVL module.
 	 * Instances of the module specific <code>SearchTree</code>,
-	 * <code>Controller</code> and  <code>GUIController</code> are created here.
+	 * <code>Controller</code> and <code>GUIController</code> are created
+	 * here.
 	 * 
 	 * @see IModuleConnector
 	 */
-	public ModuleConnector(
-		ApplicationWindow appWin,
-		Composite comp,
-		SubMenuManager menu,
-		SubToolBarManager tb,
-		SubStatusLineManager sl) {
+	public ModuleConnector(ApplicationWindow appWin, Composite comp,
+		SubMenuManager menu, SubToolBarManager tb, SubStatusLineManager sl) {
 
 		this.menuManager = menu;
 		this.toolBarManager = tb;
@@ -83,16 +87,17 @@ implements IModuleConnector {
 
 		tree = new SearchTree();
 		controller = new Controller(tree);
-		gui = new GUIController(this, appWin, comp, menu, tb, sl, controller, tree);
+		gui = new GUIController(this, appWin, comp, menu, tb, sl, controller,
+			tree);
 	}
 
-    /**
-     * The "program code" of the AVL module. Currently there is only the welcome
-     * screen displayed.
-     * 
-     * @see org.jalgo.main.IModuleConnector#run()
-     */
-    public void run() {
+	/**
+	 * The "program code" of the AVL module. Currently there is only the welcome
+	 * screen displayed.
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#run()
+	 */
+	public void run() {
 		gui.installWelcomeScreen();
 	}
 
@@ -102,99 +107,143 @@ implements IModuleConnector {
 	 * asked for saving his work.
 	 * 
 	 * @return <code>true</code>, if module is ready to be closed,
-	 * 			<code>false</code> otherwise
+	 *         <code>false</code> otherwise
 	 */
 	public boolean close() {
-		//if some dialogs are open, close this first
+		// if some dialogs are open, close this first
 		if (gui.isDialogOpen()) return false;
 
-		//ensure, that tree is in consistent state
+		// ensure, that tree is in consistent state
 		if (controller.algorithmHasNextStep()) try {
 			controller.abort();
 			gui.algorithmAborted();
 		}
-		catch (NoActionException ex) {}
+		catch (NoActionException ex) {
+			// no exception handling here, module is being closed
+		}
 
-		if (gui.areChangesToSave() && !gui.showSaveDialog()) return false;
 		return true;
 	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#setDataFromFile(java.io.ByteArrayInputStream)
-     */
-    public void setDataFromFile(ByteArrayInputStream data) {
-    	try {
-    		ObjectInputStream in = new ObjectInputStream(data);
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#setDataFromFile(java.io.ByteArrayInputStream)
+	 */
+	@SuppressWarnings("unchecked")
+	public void setDataFromFile(ByteArrayInputStream data) {
+		try {
+			ObjectInputStream in = new ObjectInputStream(data);
 			boolean avlMode = in.readBoolean();
 			gui.setAVLMode(avlMode, avlMode);
 			tree.importLevelOrder((List<Integer>)in.readObject());
 			gui.installStandardLayout();
-    	}
+		}
 		catch (IOException ex) {
-			gui.showErrorMessage("Keine gültige AVL-Datei.");
+			gui.showErrorMessage(Messages.getString(
+				"avl", "ModuleConnector.No_valid_AVL_file")); //$NON-NLS-1$ //$NON-NLS-2$
 		}
 		catch (ClassNotFoundException ex) {
-			gui.showErrorMessage("Fehler beim Laden der Datei.\r\n"+
-				"Die Datei ist möglicherweise beschädigt.");
+			gui.showErrorMessage(Messages.getString(
+				"avl", "ModuleConnector.Loading_error") + //$NON-NLS-1$ //$NON-NLS-2$
+				System.getProperty("line.separator") + //$NON-NLS-1$
+				Messages.getString("avl", "ModuleConnector.File_damaged")); //$NON-NLS-1$ //$NON-NLS-2$
 		}
-    }
+	}
 
-    /**
-     * Here the data for serializing the <code>SearchTree</code> is formatted.
-     * First there is a boolean value serialized, indicating, if the tree is an
-     * AVL-tree. Secondly the integer keys of the <code>Node</code>s are serialized
-     * in a levelordered linked list.
-     * 
-     * @see org.jalgo.main.IModuleConnector#getDataForFile()
-     */
-    public ByteArrayOutputStream getDataForFile() {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        try {
-        	ObjectOutputStream objOut = new ObjectOutputStream(out);
+	/**
+	 * Here the data for serializing the <code>SearchTree</code> is formatted.
+	 * First there is a boolean value serialized, indicating, if the tree is an
+	 * AVL-tree. Secondly the integer keys of the <code>Node</code>s are
+	 * serialized in a levelordered linked list.
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getDataForFile()
+	 */
+	public ByteArrayOutputStream getDataForFile() {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		try {
+			ObjectOutputStream objOut = new ObjectOutputStream(out);
 			objOut.writeBoolean(controller.isAVLMode());
-            List<Node> nodes = tree.exportLevelOrder();
+			List<Node> nodes = tree.exportLevelOrder();
 			List<Integer> keys = new LinkedList<Integer>();
-			for (Node node : nodes) keys.add(node.getKey());
+			for (Node node : nodes)
+				keys.add(node.getKey());
 			objOut.writeObject(keys);
-            objOut.close();
-			gui.setChangesToSave(false);
-        }
-        catch(IOException ex) {ex.printStackTrace();}
-        return out;
-    }
+			objOut.close();
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		return out;
+	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#print()
-     */
-    public void print() {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#print()
+	 */
+	public void print() {
+	// printing is currently not supported
+	}
 
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getMenuManager()
+	 */
+	public SubMenuManager getMenuManager() {
+		return menuManager;
+	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#getMenuManager()
-     */
-    public SubMenuManager getMenuManager() {
-    	return menuManager;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getToolBarManager()
+	 */
+	public SubToolBarManager getToolBarManager() {
+		return toolBarManager;
+	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#getToolBarManager()
-     */
-    public SubToolBarManager getToolBarManager() {
-        return toolBarManager;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getStatusLineManager()
+	 */
+	public SubStatusLineManager getStatusLineManager() {
+		return statusLineManager;
+	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#getStatusLineManager()
-     */
-    public SubStatusLineManager getStatusLineManager() {
-        return statusLineManager;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getModuleInfo()
+	 */
+	public IModuleInfo getModuleInfo() {
+		return moduleInfo;
+	}
 
-    /* (non-Javadoc)
-     * @see org.jalgo.main.IModuleConnector#getModuleInfo()
-     */
-    public IModuleInfo getModuleInfo() {
-        return moduleInfo;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#getSaveStatus()
+	 */
+	public int getSaveStatus() {
+		if ((saveStatus & SAVING_BLOCKED) != 0) return SAVING_BLOCKED;
+		return saveStatus;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.jalgo.main.IModuleConnector#setSaveStatus(int)
+	 */
+	public void setSaveStatus(int status) {
+		this.saveStatus = status;
+		JAlgoGUIConnector.getInstance().saveStatusChanged(this);
+	}
+
+	public void setSavingBlocked(boolean blocked) {
+		if (blocked) setSaveStatus(saveStatus | SAVING_BLOCKED);
+		else setSaveStatus(saveStatus & ~SAVING_BLOCKED);
+	}
 }
